4ec8897efe7eb065528ee12ce65d9b737e949952
yousufislamme <online.okk@gmail.com>
2023-11-11 15:00:18 +0600

    all calculate is done

---
+++ components/Home/Home.jsx
@@ -0,0 +1,131 @@
+"use client"
+
+import { useState } from "react";
+   
+const Hero = () => {  
+   const [bank, setBank] = useState(0);
+   const [optionValueProvider, setOptionValueProvider] = useState();
+   const [optionIdProvider, setOptionIdProvider] = useState();
+   const [amount, setAmount] = useState();
+   const [expenseAmount, setExpenseAmount] = useState(0); 
+   
+   // handle input
+   const handleInput = (e) => {
+      let inputText = e.target.value;
+      const inputStrToNum = parseFloat(inputText);
+      setAmount(inputStrToNum); 
+   }
+   
+   // handle submit
+   const handleSubmit = (e) => {
+      // const eatingCost = amount;
+      e.preventDefault(); 
+
+
+            // condition
+      if ('addMoneyId' === optionIdProvider) {
+         const addMoneyCalculate = bank + amount;
+         setBank(addMoneyCalculate);
+         console.log('addMoney id is connected ', optionIdProvider);
+      } else if ('productId' === optionIdProvider) {
+         const productCalculate = expenseAmount + amount;
+         setExpenseAmount(productCalculate);
+      }else if ('withdrawalId' === optionIdProvider) {
+         const withdrawalCalculate = bank - amount;
+         setBank(withdrawalCalculate);
+      } else if ('cashId' === optionIdProvider) {
+         const cashCalculate = bank + amount;
+         setBank(cashCalculate);
+      } else if ('eatingId' === optionIdProvider && amount < bank) {
+         const eatingCalculate = amount + expenseAmount;
+        setExpenseAmount(eatingCalculate);  
+      } else {
+         alert('amount is low');
+         console.log('id is not connected ', optionIdProvider);
+
+      }
+
+      console.log(optionIdProvider, amount);
+   }
+
+   // handle change option
+   const handleChangeOption = (e) => {
+      const optionID = e.target.options[e.target.selectedIndex].id;
+      setOptionIdProvider(optionID); // for id provider
+      const optionValue = optionID.value;
+      setOptionValueProvider(optionValue); // for value provider
+      console.log(optionValue); 
+   }
+   const currentBalanceIs = bank - expenseAmount;
+
+   return (
+      
+      <section className="m-2 myRounded bg-gray-100">
+
+           <div className="grid grid-cols-5 text-center gap-2 p-2">
+               <div className="h-36 myRounded myFontStyle col-span-5 bg-gradient-to-b from-purple-400 to-purple-500">
+                  <h3 className="text-2xl text-purple-900">Total Budget Bank</h3>
+               <p><span>$</span>{bank}</p>
+               </div> 
+              <div className="col-span-3 myRounded myFontStyle bg-gradient-to-r from-sky-400 to-sky-600">
+                 <h3 className="text-sky-900">Expense</h3>
+                 <p>
+                  <span>${expenseAmount }</span>
+                  
+                 </p>
+              </div>
+              <div className="col-span-2 myRounded myFontStyle bg-green-400">
+                 <h3 className="text-green-900">Today now </h3>
+               <p><span>$</span>{currentBalanceIs}</p>
+              </div>
+           </div>     
+           <div className="w-full flex justify-center gap-2 ">
+            <form  
+               onChange={()=>handleSubmit}
+               >
+                  <div className='flex flex-col justify-center items-center gap-y-3'>
+                     
+                  <h3 className='myFontStyle text-gray-700'>Now Send</h3>
+                  <p>{ amount}</p>
+                   
+                     <div className="flex justify-center flex-col text-center">
+
+                     <select
+                        id="items"
+                        value={optionValueProvider}
+                        onChange={handleChangeOption}
+                     >
+                           <option id="productId" value="Products">Products</option>
+                           <option id="addMoneyId" value="Add Money">Add Money</option>
+                           <option id="withdrawalId" value="Withdrawal">Withdrawal</option>
+                           <option id="cashId" value="Cash">Cash</option>
+                           <option id="eatingId" value="Eating">Eating</option>
+                        </select>
+                     </div>
+                     <input 
+                        type="number"
+                        placeholder="Enter Amount"
+                        onChange={handleInput}
+                        className='outline-none myRounded font-semibold shadow-lg'
+                        
+                        />
+                     <button
+                           type="submit" 
+                           onClick={handleSubmit}
+                           className='bg-orange-500 shadow-lg myRounded font-semibold focus:bg-red-500 mb-5' 
+                     >Submit
+                     </button>   
+                 
+                  </div>
+                  </form>
+            </div>
+            
+             
+        
+
+     </section>
+           
+  )
+}
+
+export default Hero;
---
+++ components/Transition.jsx
@@ -0,0 +1,52 @@
+
+const Transition = () => {
+
+
+
+  return (
+    <main className="m-2">
+      <section className="SecContainer bg-gradient-to-b from-pink-100 to-pink-200 h-screen myRounded space-y-3 py-2">
+          <div className="flex justify-center items-center">
+            <h2 className="font-bold text-2xl text-pink-900">Transition</h2>
+          </div>
+        <div className="flex justify-center items-center ">
+          {/* All transition lists */}
+          <div className="flex flex-col gap-2 justify-center items-center">
+            <div className="flex justify-between w-[350px] px-5 py-2 myRounded bg-white">
+              <div>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M14.25 6.087c0-.355.186-.676.401-.959.221-.29.349-.634.349-1.003 0-1.036-1.007-1.875-2.25-1.875s-2.25.84-2.25 1.875c0 .369.128.713.349 1.003.215.283.401.604.401.959v0a.64.64 0 01-.657.643 48.39 48.39 0 01-4.163-.3c.186 1.613.293 3.25.315 4.907a.656.656 0 01-.658.663v0c-.355 0-.676-.186-.959-.401a1.647 1.647 0 00-1.003-.349c-1.036 0-1.875 1.007-1.875 2.25s.84 2.25 1.875 2.25c.369 0 .713-.128 1.003-.349.283-.215.604-.401.959-.401v0c.31 0 .555.26.532.57a48.039 48.039 0 01-.642 5.056c1.518.19 3.058.309 4.616.354a.64.64 0 00.657-.643v0c0-.355-.186-.676-.401-.959a1.647 1.647 0 01-.349-1.003c0-1.035 1.008-1.875 2.25-1.875 1.243 0 2.25.84 2.25 1.875 0 .369-.128.713-.349 1.003-.215.283-.4.604-.4.959v0c0 .333.277.599.61.58a48.1 48.1 0 005.427-.63 48.05 48.05 0 00.582-4.717.532.532 0 00-.533-.57v0c-.355 0-.676.186-.959.401-.29.221-.634.349-1.003.349-1.035 0-1.875-1.007-1.875-2.25s.84-2.25 1.875-2.25c.37 0 .713.128 1.003.349.283.215.604.401.96.401v0a.656.656 0 00.658-.663 48.422 48.422 0 00-.37-5.36c-1.886.342-3.81.574-5.766.689a.578.578 0 01-.61-.58v0z" />
+                </svg>
+              </div>
+              <h2><input type="text" placeholder="Enter Title" className="text-sm" /></h2>
+              <div>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" /></svg>
+              </div>
+
+            </div>
+            <div className="flex justify-between w-[350px] px-5 py-2 myRounded bg-white">
+              <div>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 10.5V6a3.75 3.75 0 10-7.5 0v4.5m11.356-1.993l1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 01-1.12-1.243l1.264-12A1.125 1.125 0 015.513 7.5h12.974c.576 0 1.059.435 1.119 1.007zM8.625 10.5a.375.375 0 11-.75 0 .375.375 0 01.75 0zm7.5 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
+              </svg>
+
+              </div>
+              <h2><input type="text" placeholder="Enter Title" className="text-sm" /></h2>
+              <div>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" /></svg>
+              </div>
+
+            </div>
+
+
+          </div>
+
+        </div>
+      </section>
+    </main>
+  )
+}
+
+export default Transition;
