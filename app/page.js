e7a3125f9bb556d96d8514ebb3a38822c3d96c7c
yousufislamme <online.okk@gmail.com>
2024-02-18 23:33:22 +0600

    change and update

--- app/page.js
+++ app/page.js
@@ -2,11 +2,13 @@
 import Transition from "@/components/Transition/Transition";
 import Hero from "../components/Home/Home";
 import { MyContextProvider } from "@/context/MyDataContext";
+import Header from "@/components/Header";
 
 export default function Home() {
   return (
     <main className="m-auto max-w-xl">
       <MyContextProvider>
+        <Header />
         <Hero />
         <Transition />
       </MyContextProvider>
